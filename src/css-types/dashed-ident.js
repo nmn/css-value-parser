c83bff638944e33a66bb4dc7979888e42b41bec9
Naman Goel <nmn@fb.com>
2023-05-01 17:42:28 -0700

    Add <dashed-ident> type and tests

---
+++ src/css-types/dashed-ident.js
@@ -0,0 +1,18 @@
+// @flow strict
+
+import { Parser } from "../core";
+import { CustomIdentifier } from "./custom-ident";
+
+export class DashedIdentifier {
+  +value: string;
+
+  constructor(value: string) {
+    this.value = value;
+  }
+
+  static get parse(): Parser<DashedIdentifier> {
+    return Parser.sequence(Parser.string("--"), CustomIdentifier.parse).map(
+      ([dash, ident]) => new DashedIdentifier(dash + ident.value)
+    );
+  }
+}
